a0cdaa0769f869eee3c5182ccbcecf1c0863a216
sachrekar21 <sachrekar@cigital.com>
2019-03-28 13:53:22 -0400

    Update index.js

--- models/index.js
+++ models/index.js
@@ -3,11 +3,17 @@ const fs = require('fs')
 const path = require('path')
 const sequelizeNoUpdateAttributes = require('sequelize-noupdate-attributes')
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize('database', 'username', 'password', {
-  dialect: 'sqlite',
-  storage: 'data/juiceshop.sqlite',
-  logging: false
-  // operatorsAliases: true
+const sequelize = new Sequelize('juiceshop', 'admin', 'C1g1ta!2019', {
+    host: 'juiceshop.ckkazoopx6gq.us-east-2.rds.amazonaws.com',
+    port: 5432,
+    logging: console.log,
+    maxConcurrentQueries: 100,
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl:'Amazon RDS'
+    },
+    pool: { maxConnections: 5, maxIdleTime: 30},
+    language: 'en'
 })
 sequelizeNoUpdateAttributes(sequelize)
 const db = {}
